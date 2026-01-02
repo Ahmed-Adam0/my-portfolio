df4f8b0223adebf2f9c5045e9cb0e07c31ca1b18
Ahmed Adam Mahmoud <133882777+Ahmed-Adam0@users.noreply.github.com>
2026-01-02 17:48:39 +0200

    my-portfolio editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from "./features/hero/hero.component";
-import { AboutComponent } from "./features/about/about.component";
-import { SkillsComponent } from "./features/skills/skills.component";
 import { ProjectsComponent } from "./features/projects/projects.component";
 import { ContactComponent } from "./features/contact/contact.component";
 //import { RegisterComponent } from "./features/auth/pages/register/register.component";
@@ -10,7 +8,7 @@ import { ContactComponent } from "./features/contact/contact.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [HeroComponent,  ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
