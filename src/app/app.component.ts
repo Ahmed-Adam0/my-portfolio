9bac31707d56a7b6afd969a4a2ab5459820a5374
Ahmed Adam Mahmoud <133882777+Ahmed-Adam0@users.noreply.github.com>
2026-01-02 15:44:03 +0200

    Responsive Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./features/hero/hero.component";
+import { AboutComponent } from "./features/about/about.component";
+import { SkillsComponent } from "./features/skills/skills.component";
+import { ProjectsComponent } from "./features/projects/projects.component";
+import { ContactComponent } from "./features/contact/contact.component";
+//import { RegisterComponent } from "./features/auth/pages/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
